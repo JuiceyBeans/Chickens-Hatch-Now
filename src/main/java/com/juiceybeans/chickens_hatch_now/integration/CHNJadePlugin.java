538d4c804c2dd260f7273fd0d420357fcfb3b2f5
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-03-22 18:46:03 +0000

    rc-1

--- src/main/java/com/juiceybeans/chickens_hatch_now/integration/CHNJadePlugin.java
+++ src/main/java/com/juiceybeans/chickens_hatch_now/integration/CHNJadePlugin.java
@@ -1,22 +1,20 @@
 package com.juiceybeans.chickens_hatch_now.integration;
 
-/*import net.minecraft.world.level.block.Block;
-
-import snownee.jade.api.IWailaClientRegistration;
-import snownee.jade.api.IWailaCommonRegistration;
-import snownee.jade.api.IWailaPlugin;
-import snownee.jade.api.WailaPlugin;
-
-@WailaPlugin
-public class CHNJadePlugin implements IWailaPlugin {
-
-    @Override
-    public void register(IWailaCommonRegistration registration) {
-        registration.registerBlockDataProvider(new EggInfoDataProvider(), Block.class);
-    }
-
-    @Override
-    public void registerClient(IWailaClientRegistration registration) {
-        registration.registerBlockComponent(new EggInfoComponentProvider(), Block.class);
-    }
-}*/
+/*
+ * import net.minecraft.world.level.block.Block;
+ * 
+ * 
+ * @WailaPlugin
+ * public class CHNJadePlugin implements IWailaPlugin {
+ * 
+ * @Override
+ * public void register(IWailaCommonRegistration registration) {
+ * registration.registerBlockDataProvider(new EggInfoDataProvider(), Block.class);
+ * }
+ * 
+ * @Override
+ * public void registerClient(IWailaClientRegistration registration) {
+ * registration.registerBlockComponent(new EggInfoComponentProvider(), Block.class);
+ * }
+ * }
+ */
--- src/main/java/com/juiceybeans/chickens_hatch_now/integration/EggInfoDataProvider.java
+++ src/main/java/com/juiceybeans/chickens_hatch_now/integration/EggInfoDataProvider.java
@@ -1,33 +1,26 @@
 package com.juiceybeans.chickens_hatch_now.integration;
 
-/*import com.juiceybeans.chickens_hatch_now.ChickensHatchNow;
-
-import net.minecraft.network.RegistryFriendlyByteBuf;
-import net.minecraft.network.codec.ByteBufCodecs;
-import net.minecraft.network.codec.StreamCodec;
-import net.minecraft.resources.Identifier;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-
-import org.jspecify.annotations.Nullable;
-import snownee.jade.api.BlockAccessor;
-import snownee.jade.api.StreamServerDataProvider;
-
-public class EggInfoDataProvider implements StreamServerDataProvider<BlockAccessor, Integer> {
-
-    public static final EggInfoDataProvider INSTANCE = new EggInfoDataProvider();
-
-    @Override
-    public @Nullable Integer streamData(BlockAccessor blockAccessor) {
-        return blockAccessor.getBlockState().getOptionalValue(BlockStateProperties.HATCH).orElse(null);
-    }
-
-    @Override
-    public StreamCodec<RegistryFriendlyByteBuf, Integer> streamCodec() {
-        return ByteBufCodecs.VAR_INT.cast();
-    }
-
-    @Override
-    public Identifier getUid() {
-        return ChickensHatchNow.id("egg_info");
-    }
-}*/
+/*
+ * import com.juiceybeans.chickens_hatch_now.ChickensHatchNow;
+ * 
+ * 
+ * public class EggInfoDataProvider implements StreamServerDataProvider<BlockAccessor, Integer> {
+ * 
+ * public static final EggInfoDataProvider INSTANCE = new EggInfoDataProvider();
+ * 
+ * @Override
+ * public @Nullable Integer streamData(BlockAccessor blockAccessor) {
+ * return blockAccessor.getBlockState().getOptionalValue(BlockStateProperties.HATCH).orElse(null);
+ * }
+ * 
+ * @Override
+ * public StreamCodec<RegistryFriendlyByteBuf, Integer> streamCodec() {
+ * return ByteBufCodecs.VAR_INT.cast();
+ * }
+ * 
+ * @Override
+ * public Identifier getUid() {
+ * return ChickensHatchNow.id("egg_info");
+ * }
+ * }
+ */
