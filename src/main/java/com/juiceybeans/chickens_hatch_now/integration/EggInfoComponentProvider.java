fc2a5b5ea41099459e7e08ce04145609f74a7fd9
Karthi Mol <75553966+JuiceyBeans@users.noreply.github.com>
2026-03-26 09:57:18 +0000

    Reenable Jade integration

--- src/main/java/com/juiceybeans/chickens_hatch_now/integration/EggInfoComponentProvider.java
+++ src/main/java/com/juiceybeans/chickens_hatch_now/integration/EggInfoComponentProvider.java
@@ -1,21 +1,28 @@
 package com.juiceybeans.chickens_hatch_now.integration;
 
-/*
- * import com.juiceybeans.chickens_hatch_now.ChickensHatchNow;
- * 
- * 
- * public class EggInfoComponentProvider implements IBlockComponentProvider {
- * 
- * @Override
- * public void appendTooltip(ITooltip tooltip, BlockAccessor blockAccessor, IPluginConfig iPluginConfig) {
- * Optional<Integer> hatch_progress = EggInfoDataProvider.INSTANCE.decodeFromData(blockAccessor);
- * 
- * hatch_progress.ifPresent(i -> tooltip.add(Component.translatable("chicken_hatch_now.jade.hatch_progress", i)));
- * }
- * 
- * @Override
- * public Identifier getUid() {
- * return ChickensHatchNow.id("egg_info");
- * }
- * }
- */
+import com.juiceybeans.chickens_hatch_now.ChickensHatchNow;
+
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
+
+import snownee.jade.api.BlockAccessor;
+import snownee.jade.api.IBlockComponentProvider;
+import snownee.jade.api.ITooltip;
+import snownee.jade.api.config.IPluginConfig;
+
+import java.util.Optional;
+
+public class EggInfoComponentProvider implements IBlockComponentProvider {
+
+    @Override
+    public void appendTooltip(ITooltip tooltip, BlockAccessor blockAccessor, IPluginConfig iPluginConfig) {
+        Optional<Integer> hatch_progress = EggInfoDataProvider.INSTANCE.decodeFromData(blockAccessor);
+
+        hatch_progress.ifPresent(i -> tooltip.add(Component.translatable("chicken_hatch_now.jade.hatch_progress", i)));
+    }
+
+    @Override
+    public Identifier getUid() {
+        return ChickensHatchNow.id("egg_info");
+    }
+}
